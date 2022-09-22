436163161498bacfa4c7e0e889c14b409f66b6e4
Ilya Shimbuev <iliyarock@gmail.com>
2022-09-22 23:26:52 +0300

    Added classes to configure services for IoC.

---
+++ RandomSerializerTestApp/src/RandomSerializerTestApp.Core/Configuration/CoreRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RandomSerializerTestApp.Core.LocalStorage;
+using RandomSerializerTestApp.Core.Persons;
+
+namespace RandomSerializerTestApp.Core.Configuration;
+
+public static class CoreRegistration
+{
+    public static IServiceCollection AddCore(this IServiceCollection services, string directoryNameOfFiles)
+    {
+        services
+            .AddScoped<ILocalStorageSettings>(x => new LocalStorageSettings { DirectoryNameOfFiles = directoryNameOfFiles })
+            .AddScoped<ILocalStorageService, LocalStorageService>()
+            .AddScoped<IPersonGeneratorService, PersonGeneratorService>();
+
+        return services;
+    }
+}
